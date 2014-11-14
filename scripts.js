b44684a2d3b6ca7a02c75dda3e89d1695d7d4ab9
Hill <athill@BL-UITS-ESLT165.local>
2014-11-14 15:40:37 -0500

    currently functioning. algorithms may need tweaking

--- scripts.js
+++ scripts.js
@@ -1,91 +1,115 @@
 var size = 20;
 var threshold = 0.5;
-var interval = 500;
+var interval = 50;
 var grid = [];
-var max = 100;
+var max = -1;
 var count = 0;
 
 $(function() {
 	var $game = $('#game');
-	
-	for (var i = 0; i < size; i++) {
-		var $row = $('<div class="row"></div>');
-		grid[i] = [];
-		for (var j = 0; j <  size; j++) {
-			var on = Math.random() >= threshold;
-			grid[i].push(on);
-			var clazz = (on) ? 'on' : 'off';
-			$row.append('<div class="cell '+clazz+'" id="cell_'+i+'-'+j+'"></div>');
-		}
-		$game.append($row);
-	}
-	// console.log(grid);
-	setTimeout(game.play, interval);
-	// game.play();
+		grid = [];
+		
+		for (var i = 0; i < size; i++) {
+			var $row = $('<div class="row"></div>');
+			grid[i] = [];
+			for (var j = 0; j <  size; j++) {
+				var on = (Math.random() >= threshold) ? 1 : 0;
+				grid[i].push(on);
+				var classes = 'cell';
+				if (on) classes += ' on';
+				$row.append('<div class="'+classes+'" id="cell_'+i+'-'+j+'"></div>');
+			}
+			$game.append($row);
+		}	
+
+	// setTimeout(game.play, interval);
+	$('#start').click(function(e) {
+		// test = game.begin();
+		game.runner = window.setInterval(game.play, interval);
+		
+		
+		return false;
+		console.log(interval);
+	});
+	$('#stop').click(function(e) {
+		window.clearInterval(game.runner);
+		return false;
+	});
 });
 
 var game =  {
+	runner: null,
+	$game: null,
+	grid: [],
+	// rendering: false,
+	settings: {
+		size: 20,
+		threshold: 0.5,
+		interval: null,
+		
+	},
 	play: function() {
 		console.log('playing');
 		var newgrid = [];
 		for (var i = 0; i  < size; i++) {
 			newgrid[i] = [];
 			for (var j = 0; j < size; j++) {
-				var counts = game.getCounts(i, j);
-				// console.log(grid[i][j], counts);
-				newgrid[i][j] = game.getStatus(grid[i][j], counts);
+				var alive = game.getAlive(i, j);
+				newgrid[i][j] = game.getStatus(grid[i][j], alive);
 			}
 		}
 		for (var i = 0; i < size; i++) {
 			for (var j = 0; j < size; j++) {
 				var clazz = (newgrid[i][j]) ? 'on' : 'off';
 				var $cell = $('#cell_'+i+'-'+j);
-				$cell.removeClass('on off');
-				$cell.addClass(clazz);				
+				if (newgrid[i][j]) $cell.addClass('on');
+				else $cell.removeClass('on');
+				// $cell.removeClass('on off');
+				// $cell.addClass(clazz);				
 			}
 		}
 		grid = newgrid;
-		count++;
-		if (count < max) {
-			setTimeout(game.play, interval);	
-		}
-		
+		console.log('?');
+		// count++;
+		// if (count < max || max < 0) {
+		// 	setTimeout(game.play, interval);	
+		// }
 	},
-	getStatus: function(stat, counts) {
-		if (stat) {
-			if (counts.on < 2 || counts.on > 3) return false;
-		} else {
-			if (counts.on == 3) return true;
-		}
+	begin: function(callback) {
+		// var callback = callback || game.play;
+		// // game.rendering = true;
+		// console.log('rendering');
+		// for (var i = 0; i < size; i++) {
+		// 	var $row = $('<div class="row"></div>');
+		// 	grid[i] = [];
+		// 	for (var j = 0; j <  size; j++) {
+		// 		var on = (Math.random() >= threshold) ? 1 : 0;
+		// 		grid[i].push(on);
+		// 		var classes = 'cell';
+		// 		if (on) classes += ' on';
+		// 		$row.append('<div class="'+classes+'" id="cell_'+i+'-'+j+'"></div>');
+		// 	}
+		// 	$game.append($row);
+		// }
+		// game.runner = window.setInterval(game.play, interval);
+	},
+	getStatus: function(stat, alive) {
+		if (stat && alive < 2 || alive > 3) return 0;
+		if (!stat && alive == 3) return 1;
 		return stat;
 	},
-	getCounts: function(i, j) {
-		var neighbors = [];
-		if (i > 0) {
-			var it = i - 1;
-			neighbors.push(grid[it][j]);
-			if (j > 0) neighbors.push(grid[it][j-1]);
-			if (j < size-1) neighbors.push(grid[it][j+1]);
-		}
-		if (i < size-1) {
-			var it = i+1;
-			neighbors.push(grid[it][j]);
-			if (j > 0) neighbors.push(grid[it][j-1]);
-			if (j < size-1) neighbors.push(grid[it][j+1]);
-		}		
-		if (j > 0) neighbors.push(grid[i][j-1]);	
-		if (j < size-1) neighbors.push(grid[i][j+1]);
-		var counts = {
-			on: 0,
-			off: 0
-		}
-		for (var i = 0; i < neighbors.length; i++) {
-			if (neighbors[i]) {
-				counts.on++;
-			} else {
-				counts.off++;
-			}
-		}
-		return counts;
+	getAlive: function(i, j) {
+		var ip1 = (i+1)%size;
+		var jp1 = (j+1)%size;
+		var im1 = (i == 0) ? size-1 : i-1;
+		var jm1 = (j == 0) ? size-1 : j-1;
+		return grid[i][jm1] +		//// top
+				grid[ip1][jm1] + 
+				grid[ip1][j] + 		//// right
+				grid[ip1][jp1] + 
+				grid[i][jp1] +		//// bottom
+				grid[im1][jp1] +
+				grid[im1][j] +		//// left
+				grid[im1][jm1];
 	}
 }
