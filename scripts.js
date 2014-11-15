ac29c037b11a4f7bc67ae07d7c438ab21bff04b1
Andy Hill <athill@iu.edu>
2014-11-15 11:32:15 -0500

    - Added fields for size and threshold - bootstrapped form - broke start() and stop() into methods - etc.

--- scripts.js
+++ scripts.js
@@ -1,47 +1,45 @@
-var size = 20;
-var threshold = 0.5;
-var interval = 50;
 var grid = [];
-var max = -1;
-var count = 0;
+var $game = null;
 
 $(function() {
-	var $game = $('#game');
-		grid = [];
-		
-		for (var i = 0; i < size; i++) {
-			var $row = $('<div class="row"></div>');
-			grid[i] = [];
-			for (var j = 0; j <  size; j++) {
-				var on = (Math.random() >= threshold) ? 1 : 0;
-				grid[i].push(on);
-				var classes = 'cell';
-				if (on) classes += ' on';
-				$row.append('<div class="'+classes+'" id="cell_'+i+'-'+j+'"></div>');
-			}
-			$game.append($row);
-		}	
-
-	// setTimeout(game.play, interval);
+	$game = $('#game');
+	$.each(['#interval', '#size', '#threshold'], function(i, v) {
+		game.setDisplayValue($(v));
+	})
 	$('#start').click(function(e) {
-		// test = game.begin();
-		// game.runner = window.setInterval(game.play, interval);
-		game.begin(function() {console.log('start test')});
-		
+		test = game.begin();
 		return false;
-		console.log(interval);
 	});
 	$('#stop').click(function(e) {
-		window.clearInterval(game.runner);
+		game.stop();
 		return false;
 	});
+	$('#pause').click(function(e) {
+
+		return false;
+	});
+	$('#interval').change(function(e) {
+		game.settings.interval = $('#interval').val();
+		game.setDisplayValue($(this));
+		if ($game.html() != '') {
+			game.stop();
+			game.start();
+		}
+	});
+	$('#size').change(function(e) {
+		game.settings.size = $('#size').val();
+		game.setDisplayValue($(this));
+	});	
+	$('#threshold').change(function(e) {
+		game.settings.threshold = $('#threshold').val();
+		game.setDisplayValue($(this));
+	});		
 });
 
 var game =  {
 	runner: null,
 	$game: null,
 	grid: [],
-	// rendering: false,
 	settings: {
 		size: 20,
 		threshold: 0.5,
@@ -49,8 +47,8 @@ var game =  {
 		
 	},
 	play: function() {
-		console.log('playing');
 		var newgrid = [];
+		var size = game.settings.size;
 		for (var i = 0; i  < size; i++) {
 			newgrid[i] = [];
 			for (var j = 0; j < size; j++) {
@@ -60,39 +58,42 @@ var game =  {
 		}
 		for (var i = 0; i < size; i++) {
 			for (var j = 0; j < size; j++) {
-				var clazz = (newgrid[i][j]) ? 'on' : 'off';
 				var $cell = $('#cell_'+i+'-'+j);
 				if (newgrid[i][j]) $cell.addClass('on');
 				else $cell.removeClass('on');
-				// $cell.removeClass('on off');
-				// $cell.addClass(clazz);				
 			}
 		}
 		grid = newgrid;
-		console.log('?');
-		// count++;
-		// if (count < max || max < 0) {
-		// 	setTimeout(game.play, interval);	
-		// }
 	},
 	begin: function(callback) {
 		var callback = callback || game.play;
-		// // game.rendering = true;
-		// console.log('rendering');
-		// for (var i = 0; i < size; i++) {
-		// 	var $row = $('<div class="row"></div>');
-		// 	grid[i] = [];
-		// 	for (var j = 0; j <  size; j++) {
-		// 		var on = (Math.random() >= threshold) ? 1 : 0;
-		// 		grid[i].push(on);
-		// 		var classes = 'cell';
-		// 		if (on) classes += ' on';
-		// 		$row.append('<div class="'+classes+'" id="cell_'+i+'-'+j+'"></div>');
-		// 	}
-		// 	$game.append($row);
-		// }
-		// game.runner = window.setInterval(game.play, interval);
-		game.runner = window.setInterval(game.play, interval);
+		$game.html("");
+		grid: [];
+		game.settings.interval = $('#interval').val();
+		game.settings.threshold = $('#threshold').val();
+		game.settings.size = $('#size').val();
+		for (var i = 0; i < game.settings.size; i++) {
+			var $row = $('<div class="row"></div>');
+			grid[i] = [];
+			for (var j = 0; j <  game.settings.size; j++) {
+				var on = (Math.random() >= game.settings.threshold) ? 1 : 0;
+				grid[i].push(on);
+				var classes = 'cell';
+				if (on) classes += ' on';
+				$row.append('<div class="'+classes+'" id="cell_'+i+'-'+j+'"></div>');
+			}
+			$game.append($row);
+		}
+		game.start();
+	},
+	start: function(callback) {
+		var callback = callback || game.play;
+
+		game.runner = window.setInterval(game.play, game.settings.interval);
+	},
+	stop: function() {
+		window.clearInterval(game.runner);
+		game.runner = null;
 	},
 	getStatus: function(stat, alive) {
 		if (stat && alive < 2 || alive > 3) return 0;
@@ -100,13 +101,11 @@ var game =  {
 		return stat;
 	},
 	getAlive: function(i, j) {
-		var jm1 = (j == 0) ? size-1 : j-1;	//// j-1 top
-		var ip1 = (i+1)%size;				//// i+1 right
-		var jp1 = (j+1)%size;				//// j+i bottom
-		var im1 = (i == 0) ? size-1 : i-1;	//// i-1 left
-		
-		
-		
+		var jm1 = (j == 0) ? game.settings.size-1 : j-1;	//// j-1 top
+		var ip1 = (i+1)%game.settings.size;				//// i+1 right
+		var jp1 = (j+1)%game.settings.size;				//// j+i bottom
+		var im1 = (i == 0) ? game.settings.size-1 : i-1;	//// i-1 left
+			
 		return grid[i][jm1] +		//// top
 				grid[ip1][jm1] + 
 				grid[ip1][j] + 		//// right
@@ -115,5 +114,9 @@ var game =  {
 				grid[im1][jp1] +
 				grid[im1][j] +		//// left
 				grid[im1][jm1];
-	}
+	},
+	setDisplayValue: function($rangeField) {
+		var $value = $('label[for="'+$rangeField.attr('id')+'"]').find('.value');
+		$value.html(' ('+$rangeField.val()+')');
+	} 
 }
